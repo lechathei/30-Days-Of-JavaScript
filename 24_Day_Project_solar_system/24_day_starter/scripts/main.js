4fc26799f9647a4b7b85413b4ec7d5c709c258e9
lechathei <gaojian1106@gmail.com>
2023-05-17 17:47:50 +0200

    cal value and change pic

--- 24_Day_Project_solar_system/24_day_starter/scripts/main.js
+++ 24_Day_Project_solar_system/24_day_starter/scripts/main.js
@@ -1,64 +1,50 @@
 
+
+let selectPlanet= document.getElementById("planet")
+
+const planetInfoNperKg = {
+    'mercury':0.38,
+    'venus':0.90,
+    'earth':1.00,
+    'moon':0.17,
+    'mars':0.38,
+    'jupiter':2.58,
+    'saturn':1.06,
+    'uranus':0.90,
+    'neptune':1.13,
+    'pluto':0.07,
+    'empty':1.00
+}
+
+for( const [key,val] of Object.entries(planetInfoNperKg)){
+    let k=key;
+    let newOption = document.createElement("option");
+    newOption.value=k;
+    newOption.text=k.toString().toUpperCase();
+    
+    selectPlanet.add(newOption,null);
+    
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    console.log("123");
-
-    const tablaWeight = {
-        mercury:0.38,
-        venus:0.90,
-        earth:1.00,
-        moon:0.17,
-        mars:0.38,
-        jupiter:2.58,
-        saturn:1.06,
-        uranus:0.90,
-        neptune:1.13,
-        pluto:0.07,
-    };
-
-    console.log(tablaWeight.mercury)
-
-    function myFunction(e) {
-        e.stopPropagation();
-        e.preventDefault();
-
-        let option1= document.getElementById("planet")
-        const planetInfoNperKg = {
-            mercury:0.38,
-            venus:0.90,
-            earth:1.00,
-            moon:0.17,
-            mars:0.38,
-            jupiter:2.58,
-            saturn:1.06,
-            uranus:0.90,
-            neptune:1.13,
-            pluto:0.07,
-        }
-        for( const [key,val] of Object.entries(planetInfoNperKg)){
-            let k=key;
-            
-            let newOption = document.createElement("option");
-            newOption.value=k;
-            
-            option1.add(newOption);
-            
-        }
+    function changePic(e){
+        let img= document.getElementById("planetImage");
         let optionValue = document.getElementById("planet").value;
-
-        
-        //planet name
-        document.getElementById("plname").innerHTML = optionValue.toUpperCase();
-        //la imagen
-        document.getElementById("planetImage").src = "./images/" + optionValue + ".png"
-
-        //input
-        let inputNum = document.getElementById("mass").value;
-
-  
         if (optionValue  == "--select planet--"){
-            document.getElementById("planetImage").src = "./images/earth.png"
-        } 
+            img.src= `./images/${empty}.png`;
+
+        } else {
+            img.src= `./images/${optionValue}.png`;
+        }
+
+    }
+    function calcForce(e){
+        let inputNum = document.getElementById("mass").value;
+        let optionValue = document.getElementById("planet").value;
+        let multiplier= planetInfoNperKg[optionValue];
+        console.log(multiplier)
+
         if (inputNum !== '' && optionValue !== "--select planet--") {
             document.querySelector(".description").classList.add("descriptionVisible")
             document.querySelector(".description").style.display = 'flex'
@@ -66,22 +52,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
             document.querySelector(".description").style.display = 'none'
         }
 
-        
-        //weight
-        let spanWeight = document.getElementById("")
-        spanWeight.innerHTML = (inputNum * tablaWeight[optionValue]).toFixed(2)
+        let spanWeight = document.getElementById("plvalue")
+        spanWeight.innerHTML = (inputNum * planetInfoNperKg[optionValue]).toFixed(2)
 
-
-
-    };
-
+    }
+    
     let btnSubmit = document.getElementById("button-submit")
-
-    btnSubmit.addEventListener("click", myFunction);
-
-
-
-
+    let setOptionValue= document.getElementById("planet");
+    btnSubmit.addEventListener("click", changePic);
+    btnSubmit.addEventListener("click",calcForce);
+    setOptionValue.addEventListener("change", changePic);
 
     
 });
